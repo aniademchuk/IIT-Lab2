19a1ea9b8e6582b10c85556dc02c6e3e9e3597ea
Demchuk Ganna <113458070+aniademchuk@users.noreply.github.com>
2023-03-12 22:08:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ TOKEN = '5993587987:AAH0yL0tqROch-LB29hvEoYNTBFjR4p_2L8'
 
 bot = telegram.Bot(TOKEN)
 
-bot.send_message(chat_id='373084248', text="I want to kiss Baba Vala")
+bot.send_message(chat_id='407712064', text="I want to kiss Baba Vala")
