6a8f6fa3f3b354d43ae64a6e0d5371a7df129452
Demchuk Ganna <113458070+aniademchuk@users.noreply.github.com>
2023-03-12 19:35:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import telegram
+import asyncio
 
-BOT_TOKEN = "AAH0yL0tqROch-LB29hvEoYNTBFjR4p_2L8"
+# Replace YOUR_TOKEN_HERE with your bot token
+bot = telegram.Bot(token='5993587987:AAH0yL0tqROch-LB29hvEoYNTBFjR4p_2L8')
 
-my_bot = telegram.Bot(BOT_TOKEN)
-my_bot.send_message(chat_id='407712064', text='Hello')
+# Replace YOUR_CHAT_ID_HERE with the chat_id of the user you want to send the message to
+chat_id = 407712064
+
+
+# Send the message "Hello" to the user with chat_id = YOUR_CHAT_ID_HERE
+async def main():
+    await bot.send_message(chat_id=chat_id, text='Hello')
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
