a9e869ddd390d6e1b8caaca198a76b9d6d09a781
Demchuk Ganna <113458070+aniademchuk@users.noreply.github.com>
2023-03-12 22:03:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import telegram
 
-# Replace YOUR_TOKEN_HERE with your bot token
-bot = telegram.Bot(token='5993587987:AAH0yL0tqROch-LB29hvEoYNTBFjR4p_2L8')
+TOKEN = '5735198574:AAGMSR1z21d1K5b8VuE8DfFotgrPf6ud3Qg'
 
-# Replace YOUR_CHAT_ID_HERE with the chat_id of the user you want to send the message to
-chat_id = 407712064
+bot = telegram.Bot(TOKEN)
 
-# Send the message "Hello" to the user with chat_id = YOUR_CHAT_ID_HERE
-bot.send_message(chat_id=chat_id, text='Hello')
+bot.send_message(chat_id='373084248', text="I don't want to live please kill me")
